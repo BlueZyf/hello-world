068758b09c3fc4ce045bac3f3bc7fcd3526d8235
BlueZyf <961867329@qq.com>
2020-07-14 17:51:53 +0800

    add bash text

---
+++ bash_helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author : zyf
+# Copyright (c) 
+# Script follows here:
+
+: '
+	This is single note
+'
+
+<<BLOCK 
+	This is the first comment
+BLOCK
+
+echo "Hello World!"
+echo "What is your name?"
+read PERSON
+echo "Hello," $PERSON""
